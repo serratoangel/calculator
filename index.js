53bc40c8447d08b5f2dbcb79e86efc97f12d51c9
Angel Serrato <serratoariasangel@gmail.com>
2022-05-30 19:07:05 -0500

    Changes to some files and add display

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
-const calculadora = new Calculadora();
+const display = new Display(displayValorAnterior, displayValorActual);
 
-console.log(calculadora.sumar(1, 2));
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
